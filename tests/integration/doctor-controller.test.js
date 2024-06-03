b4f38d6f90457054d8fd490c399f335b177cbcfd
Ihor <44delen@gmail.com>
2024-06-04 02:45:17 +0300

    Updated tests

--- tests/integration/doctor-controller.test.js
+++ tests/integration/doctor-controller.test.js
@@ -18,3 +18,64 @@ const mockAppointment = {
 	createdAt: new Date(),
 	updatedAt: new Date(),
 };
+
+const mockToken = jwt.sign(
+	{ id: mockDoctorId, role: "doctor" },
+	process.env.JWT_SECRET,
+	{
+		expiresIn: process.env.JWT_LIFETIME,
+	}
+);
+
+jest.mock("../../models/Appointment");
+
+describe("Doctor controller", () => {
+	beforeEach(() => {
+		Appointment.create.mockClear();
+		Appointment.findOne.mockClear();
+		Appointment.find.mockClear();
+		Appointment.findByIdAndDelete.mockClear();
+		Appointment.findOneAndUpdate.mockClear();
+	});
+
+	test("Get appointment", async () => {
+		Appointment.findOne.mockResolvedValue(mockAppointment);
+
+		const res = await request(app)
+			.get(`/api/doctor/${mockAppointmentId}`)
+			.set("Authorization", `Bearer ${mockToken}`);
+
+		expect(res.statusCode).toEqual(200);
+		expect(res.body).toHaveProperty("_id", mockAppointmentId.toString());
+		expect(Appointment.findOne).toHaveBeenCalledTimes(1);
+	});
+
+	test("Get all appointments", async () => {
+		Appointment.find.mockReturnValueOnce({
+			sort: jest.fn().mockReturnValueOnce([mockAppointment]),
+		});
+
+		const res = await request(app)
+			.get(`/api/doctor/`)
+			.set("Authorization", `Bearer ${mockToken}`);
+
+		expect(res.statusCode).toEqual(200);
+		expect(res.body.appointments).toHaveLength(1);
+		expect(res.body.count).toEqual(1);
+		expect(Appointment.find).toHaveBeenCalledTimes(1);
+	});
+
+	test("Update appointment", async () => {
+		const updatedAppointment = { ...mockAppointment, status: "confirmed" };
+		Appointment.findOneAndUpdate.mockResolvedValue(updatedAppointment);
+
+		const res = await request(app)
+			.patch(`/api/doctor/${mockAppointmentId}`)
+			.send({ status: "confirmed" })
+			.set("Authorization", `Bearer ${mockToken}`);
+
+		expect(res.statusCode).toEqual(200);
+		expect(res.body.appointment.status).toEqual("confirmed");
+		expect(Appointment.findOneAndUpdate).toHaveBeenCalledTimes(1);
+	});
+});
