b567c5a9bf8411ee21c378a04fb08f8b1b01683b
Ihor <44delen@gmail.com>
2024-06-14 03:15:16 +0300

    Added docs,security

--- app.js
+++ app.js
@@ -11,15 +11,40 @@ const authRouter = require("./routes/auth-route");
 const patientRouter = require("./routes/patient-route");
 const doctorRouter = require("./routes/doctor-route");
 
+//middlewares
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 const authMiddleware = require("./middleware/authentication");
 const { authorizePatient, authorizeDoctor } = require("./middleware/authorize");
 
+//docs
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./docs/swagger-output.json");
+
+//security
+const helmet = require("helmet");
+const cors = require("cors");
+const xss = require("xss-clean");
+const rateLimiter = require("express-rate-limit");
+
+app.use(
+	rateLimiter({
+		windowsMs: 10 * 60 * 1000,
+		max: 100,
+	})
+);
+app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+
+//routers
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use("/api/auth", authRouter);
 app.use("/api/patient", authMiddleware, authorizePatient, patientRouter);
 app.use("/api/doctor", authMiddleware, authorizeDoctor, doctorRouter);
 
+//error-middlewares
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
---
+++ docs/swagger.js
@@ -0,0 +1,15 @@
+const swaggerAutogen = require("swagger-autogen")();
+
+const doc = {
+	info: {
+		title: "DoctorsQueue API",
+		description:
+			"REST API application for patients to make doctor's appointments.",
+	},
+	host: "localhost:8080",
+};
+
+const outputFile = "../swagger-output.json";
+const routes = ["../app.js"];
+
+swaggerAutogen(outputFile, routes, doc);
